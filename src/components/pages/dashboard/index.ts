bab5d6887ee8bd0b23cc23a65816a84a0cfb99bd
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-21 19:32:03 +0200

    chore: update dashboard components barrel export with new profile components

--- src/components/pages/dashboard/index.ts
+++ src/components/pages/dashboard/index.ts
@@ -1,6 +1,6 @@
-export { ProfileCard }        from './ProfileCard';
-export { PlatformIdCard }     from './PlatformIdCard';
-export { VerificationStatus } from './VerificationStatus';
-export { EditProfileModal }   from './EditProfileModal';
-export { ProfileImageUpload } from './ProfileImageUpload';
-export { ChangePasswordModal } from './ChangePasswordModal';
+export { ProfileCard }          from './ProfileCard';
+export { PlatformIdCard }       from './PlatformIdCard';
+export { VerificationStatus }   from './VerificationStatus';
+export { EditProfileModal }     from './EditProfileModal';
+export { ProfileImageUpload }   from './ProfileImageUpload';
+export { ChangePasswordModal }  from './ChangePasswordModal';
