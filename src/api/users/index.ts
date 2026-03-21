086ad7f3bac710a897219abebb1ac40dbc4b800d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-21 19:32:06 +0200

    chore: add barrel export for users API files

---
+++ src/api/users/index.ts
@@ -0,0 +1,7 @@
+export { getProfileApi }             from './get-profile.api';
+export type { UserProfileResponse }  from './get-profile.api';
+export { updateProfileApi }          from './update-profile.api';
+export type { UpdateProfilePayload } from './update-profile.api';
+export { uploadProfileImageApi }     from './upload-profile-image.api';
+export { changePasswordApi }         from './change-password.api';
+export type { ChangePasswordPayload } from './change-password.api';
