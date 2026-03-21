c8420e6ff90b25635de775185cf7763ad3d62966
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-21 16:08:11 +0200

    feat: export AppToaster and toast from UI barrel

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,3 +4,4 @@ export { Button }      from './Button';
 export { Input }       from './Input';
 export { Card }        from './Card';
 export { StatusBadge } from './StatusBadge';
+export { AppToaster, toast } from './Toast';
