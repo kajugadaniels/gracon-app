9fdd7b49ba140498588a6ce8c1e34a01b0b8591e
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-05-14 23:33:19 +0200

    chore(ui): export app loading state

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,5 +4,6 @@ export { Button }      from './Button';
 export { Input }       from './Input';
 export { Card }        from './Card';
 export { StatusBadge } from './StatusBadge';
+export { AppLoadingState } from './AppLoadingState';
 export { AppToaster, toast } from './Toast';
 export { PremiumLoader }    from './Loader';
