9cb65e726cf308e20e88481d5364b5b875b7c9be
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-05-15 14:09:36 +0200

    refactor(shared): remove sidebar exports

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,4 +1,2 @@
 export { Navbar } from './Navbar';
 export { AuthProvider } from './AuthProvider';
-export { AppSidebar } from './AppSidebar';
-export { MobileMenuButton } from './MobileMenuButton';
